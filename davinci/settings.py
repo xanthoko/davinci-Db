b42520a9f770ea871010989c2f2078934c7a5eb8
xantho <konxantho@gmail.com>
2019-01-14 19:08:22 +0200

    ds

--- davinci/settings.py
+++ davinci/settings.py
@@ -32,12 +32,13 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'rest_framework',
-    'hotels'
+    'hotels','corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +120,7 @@ STATIC_ROOT = BASE_DIR + '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer', )
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'http://davincihotels.surge.sh',
+)
